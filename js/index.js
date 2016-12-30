ca820d7cba1e7690204929a9009946b6982c50af
Oscar Pérez <mr.omiguelperez@gmail.com>
2016-12-30 10:01:21 -0500

    Crear funcionalidad para renderizar shows y listar los buscados

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
 $(function () {
 
-  $('#app-body')
-    .find('form')
-    .submit(function (e) {
-      e.preventDefault()
-
-      var busqueda = $(this)
-        .find('input[type="text"]')
-        .val()
-      alert(`Se ha buscado ${busqueda}`)
-    })
-
+  var $tvShowsContainer = $('#app-body').find('.tv-shows')
   var template = '<article class="tv-show">' +
       '<div class="left img-container">' +
         '<img src=":img:" alt=":img alt:" />' +
@@ -19,21 +9,49 @@ $(function () {
         '<h3>:name:</h3>' +
         '<p>:summary:</p>' +
       '</div>' +
-    '</article>' +
+    '</article>'
+
+  function renderShows (shows) {
+    shows.forEach(function (show) {
+      var article = template
+        .replace(':name:', show.name)
+        .replace(':img:', show.image ? show.image.medium : '')
+        .replace(':img alt:', show.name + ' Logo')
+        .replace(':summary:', show.summary)
+      $tvShowsContainer.append($(article))
+    })
+  }
+
+  $('#app-body')
+    .find('form')
+    .submit(function (e) {
+      e.preventDefault()
+
+      var busqueda = $(this)
+        .find('input[type="text"]')
+        .val()
+
+      $tvShowsContainer.find('.tv-show').remove()
+      var $loader = $('<div class="loader">')
+      $loader.appendTo($tvShowsContainer)
+
+      $.ajax({
+        url: `http://api.tvmaze.com/search/shows?q=${busqueda}`,
+        success: function (showsContainer, textStatus, xhr) {
+          $loader.remove()
+          var shows = showsContainer.map(function (element) {
+            return element.show
+          })
+          renderShows(shows)
+        }
+      })
+    })
 
   $.ajax({
     url: 'http://api.tvmaze.com/shows',
-    method: 'get',
     success: function (shows, textStatus, xhr) {
-      var tvShowsContainer = $('#app-body').find('.tv-shows')
-      shows.forEach(function (show) {
-        var article = template
-          .replace(':name:', show.name)
-          .replace(':img:', show.image.medium)
-          .replace(':img alt:', show.name + ' Logo')
-          .replace(':summary:', show.summary)
-        tvShowsContainer.append($(article))
-      })
+      $tvShowsContainer.find('.loader').remove()
+      renderShows(shows)
     }
   })
 })
