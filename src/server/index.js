cee4b6e193431a730849bb96d89aa5ba647e6824
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-25 08:22:54 -0500

    Migrar servidor a es2015

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,19 @@
 'use strict'
 
+import express from 'express'
+import http from 'http'
+
 const port = process.env.PORT || 3000
-
-const express = require('express')
-const http = require('http')
-
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/shows', function (req, res) {
+app.get('/shows', (req, res) => {
 
 })
 
-app.post('/show', function (req, res) {
+app.post('/show', (req, res) => {
 
 })
 
-app.listen(port, function () {
-  console.log(`Server running at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
