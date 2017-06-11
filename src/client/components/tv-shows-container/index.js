3a9f9bfb55b6bfae034f0f24ccd0076855096c06
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-06-11 12:47:39 -0500

    Implementar socket.io para votar en tiempo real

--- src/client/components/tv-shows-container/index.js
+++ src/client/components/tv-shows-container/index.js
@@ -1,20 +1,23 @@
 import $ from 'jquery'
+import io from 'socket.io-client'
 
 const $tvShowsContainer = $('#app-body').find('.tv-shows')
+
+let socket = io.connect()
 
 $tvShowsContainer.on('click', 'button.like', function (e) {
   let $this = $(this)
   let $article = $this.closest('.tv-show')
   let id = $article.data('id')
 
-  $.post(`/api/vote/${id}`, () => {
-    let $counter = $article.find('.count')
-    let content = $counter.html()
-    let count = Number(content)
-    count += 1
-    $counter.html(count)
-    $article.toggleClass('liked')
-  })
+  socket.emit('vote', id)
 })
 
+socket.on('vote:done', vote => {
+  let $show = $tvShowsContainer.find('.tv-show[data-id=' + vote.showId + ']')
+  let $counter = $show.find('.count')
+  $counter.html(vote.count)
+
+  $show.toggleClass('liked')
+})
 export default $tvShowsContainer
--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,31 @@
 
 import express from 'express'
 import http from 'http'
+import socketio from 'socket.io'
 import mongoose from 'mongoose'
 import api from 'src/server/api'
+import { incrementVote } from 'src/server/lib'
 
 mongoose.connect('mongodb://localhost/tvify')
 
 const port = process.env.PORT || 3000
+
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log(`Connected: ${socket.id}`)
+
+  socket.on('vote', id => {
+    incrementVote(id, (err, vote) => {
+      if (err) socket.emit('vote:error', err)
+
+      socket.emit('vote:done', vote)
+      socket.broadcast.emit('vote:done', vote)
+    })
+  })
+})
 
 app.use(express.static('public'))
 app.use('/api', api)
